cec48cbefec681d58c38257793179779af3556a3
micha <michaclifton@yahoo.com>
2022-10-19 15:12:05 -0700

    Added Asserts to JobTest, in progress finishing

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -22,4 +22,90 @@ public class Job {
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency){
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Job job = (Job) object;
+        return id == job.id;
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(super.hashCode(), id);
+    }
+
+    public String getName() {
+        if (name == null || name.isEmpty()){
+            return "Data not available.";
+        }
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    //    @Override
+//    public boolean equals(Object o){
+//        if (this == o) return true;
+//        if (!(o instanceof Job)) return false;
+//        Job job = (Job) o;
+//        return id == job.id;
+//    }
+//    @Override
+//    public int hashCode() {return Objects.hash(id);}
+
 }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,46 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import org.launchcode.techjobs.oo.*;
+// took corecompetency off and added *
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+//had to added import static for all assert declared above.
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    public JobTest(){
+        return JobTest;
+    }
+
+    @Test
+    public void testSettingJobId(){
+        Job Job1 = new Job();
+        Job Job2 = new Job();
+        assertNotEquals(Job1.getId(), Job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job Job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(Job3.getString() instanceof String);
+        assertTrue(Job3.getEmployer() instanceof Employer);
+        assertTrue(Job3.getLocation() instanceof Location);
+        assertTrue(Job3.getPositionType() instanceof PositionType);
+        assertTrue(Job3.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals()
+    }
+
+
+
 
 }
